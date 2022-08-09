fee41cd49fb9df0bcc18d0f7937b27c942aa7032
Nikunj Thakur <nik.thak22@gmail.com>
2022-08-09 14:35:57 +0100

    First Commit

---
+++ app/src/main/java/com/example/myfirstappclock/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myfirstappclock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextClock;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickGermany(View view) {
+        TextClock clock = findViewById(R.id.my_clock);
+        clock.setTimeZone("Europe/Berlin");
+    }
+
+    public void onClickIndia(View view) {
+        TextClock clock = findViewById(R.id.my_clock);
+        clock.setTimeZone("Asia/Kolkata");
+    }
+
+    public void onClickUSA(View view) {
+        TextClock clock = findViewById(R.id.my_clock);
+        clock.setTimeZone("America");
+    }
+}
